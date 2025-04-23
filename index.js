a01d9ca7170cae52ca44e75cfe30ce6b86cc8491
Mariano Morales <102705056+msleazy@users.noreply.github.com>
2025-04-23 14:28:51 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+
+
+// Este código se ejecuta cuando el DOM está completamente cargado
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM cargado');
+
+    // Obtenemos los elementos del DOM
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const table = document.getElementById('table');
+    const alert = document.getElementById('alert');
+
+    const btn = document.getElementById('add');
+
+
+    function addTodo() {
+        console.log('agregar todo');
+        console.log(title.value);
+        console.log(description.value);
+
+        if (title.value === '' || description.value === '') {
+            alert.classList.remove('d-none');
+            alert.innerText = 'Por favor, completa todos los campos';
+            return;
+        }
+
+        alert.classList.add('d-none');
+        const row = table.insertRow();
+
+        row.innerHTML = `
+        <td>${title.value}</td>
+        <td>${description.value}</td>
+        <td class="text-center">
+          <input type="checkbox">
+        </td>
+        <td class="text-right">
+            <button class="btn btn-primary mb-1">
+                <i class="fa fa-pencil"></i>
+            </button>
+            <button class="btn btn-danger mb-1">
+                <i class="fa fa-trash"></i>
+            </button>
+        </td>`;
+
+
+
+    }
+
+    
+    btn.onclick = addTodo;
+
+});
+
+
+
+
+// btn.addEventListener('click', () => {
+//     console.log('boton agregar');
+// })
